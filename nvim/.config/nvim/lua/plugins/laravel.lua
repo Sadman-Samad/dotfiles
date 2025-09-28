c4efce6c00fe3e837addc09020da82067c60189e
Faisal Kabir Galib <faisalkabirgalib@gmail.com>
2025-09-28 17:26:01 +0600

    change laravel plugin

--- nvim/.config/nvim/lua/plugins/laravel.lua
+++ nvim/.config/nvim/lua/plugins/laravel.lua
@@ -1,108 +1,111 @@
 return {
-  "adalessa/laravel.nvim",
+  "adibhanna/laravel.nvim",
   dependencies = {
-    "tpope/vim-dotenv",
     "MunifTanjim/nui.nvim",
     "nvim-lua/plenary.nvim",
-    "nvim-neotest/nvim-nio",
-    "ravitemer/mcphub.nvim", -- optional
+    "nvim-telescope/telescope.nvim",
   },
-  cmd = { "Laravel" },
+  ft = { "php", "blade" },
   keys = {
+    { "<leader>L", "", desc = "+Laravel" },
+    { "<leader>Lc", "<cmd>Composer<cr>", desc = "Composer Commands" },
+    { "<leader>Lm", "<cmd>LaravelMake<cr>", desc = "Laravel Make" },
+    { "<leader>Lr", "<cmd>LaravelRoute<cr>", desc = "Laravel Routes" },
+    { "<leader>Ld", "<cmd>LaravelDumps<cr>", desc = "Laravel Dumps" },
+    { "<leader>Li", "<cmd>LaravelIdeHelper all<cr>", desc = "Laravel IDE Helper" },
+    { "<leader>Lgd", "<cmd>LaravelGoto<cr>", desc = "Laravel Goto" },
     {
-      "<leader>ll",
+      "<leader>La",
       function()
-        Laravel.pickers.laravel()
-      end,
-      desc = "Laravel: Open Laravel Picker",
-    },
-    {
-      "<c-g>",
-      function()
-        Laravel.commands.run("view:finder")
-      end,
-      desc = "Laravel: Open View Finder",
-    },
-    {
-      "<leader>la",
-      function()
-        Laravel.pickers.artisan()
-      end,
-      desc = "Laravel: Open Artisan Picker",
-    },
-    {
-      "<leader>lt",
-      function()
-        Laravel.commands.run("actions")
-      end,
-      desc = "Laravel: Open Actions Picker",
-    },
-    {
-      "<leader>lr",
-      function()
-        Laravel.pickers.routes()
-      end,
-      desc = "Laravel: Open Routes Picker",
-    },
-    {
-      "<leader>lh",
-      function()
-        Laravel.run("artisan docs")
-      end,
-      desc = "Laravel: Open Documentation",
-    },
-    {
-      "<leader>lm",
-      function()
-        Laravel.pickers.make()
-      end,
-      desc = "Laravel: Open Make Picker",
-    },
-    {
-      "<leader>lc",
-      function()
-        Laravel.pickers.commands()
-      end,
-      desc = "Laravel: Open Commands Picker",
-    },
-    {
-      "<leader>lo",
-      function()
-        Laravel.pickers.resources()
-      end,
-      desc = "Laravel: Open Resources Picker",
-    },
-    {
-      "<leader>lp",
-      function()
-        Laravel.commands.run("command_center")
-      end,
-      desc = "Laravel: Open Command Center",
-    },
-    {
-      "gf",
-      function()
-        local ok, res = pcall(function()
-          if Laravel.app("gf").cursorOnResource() then
-            return "<cmd>lua Laravel.commands.run('gf')<cr>"
+        local pickers = require("telescope.pickers")
+        local finders = require("telescope.finders")
+        local conf = require("telescope.config").values
+        local actions = require("telescope.actions")
+        local action_state = require("telescope.actions.state")
+
+        -- Get artisan commands
+        local handle = io.popen("php artisan list --format=json 2>/dev/null")
+        local result = handle:read("*a")
+        handle:close()
+
+        local commands = {}
+        if result and result ~= "" then
+          local ok, data = pcall(vim.fn.json_decode, result)
+          if ok and data.commands then
+            for _, cmd in pairs(data.commands) do
+              if not cmd.hidden then
+                table.insert(commands, {
+                  name = cmd.name,
+                  description = cmd.description or "",
+                })
+              end
+            end
           end
-        end)
-        if not ok or not res then
-          return "gf"
         end
-        return res
+
+        -- Fallback if json parsing fails
+        if #commands == 0 then
+          local common_commands = {
+            { name = "make:controller", description = "Create a new controller class" },
+            { name = "make:model", description = "Create a new Eloquent model class" },
+            { name = "make:migration", description = "Create a new database migration" },
+            { name = "make:seeder", description = "Create a new seeder class" },
+            { name = "make:factory", description = "Create a new model factory" },
+            { name = "make:middleware", description = "Create a new middleware class" },
+            { name = "make:request", description = "Create a new form request class" },
+            { name = "migrate", description = "Run the database migrations" },
+            { name = "migrate:rollback", description = "Rollback the last database migration" },
+            { name = "migrate:refresh", description = "Reset and re-run all migrations" },
+            { name = "tinker", description = "Interact with your application" },
+            { name = "serve", description = "Serve the application on the PHP development server" },
+            { name = "queue:work", description = "Start processing jobs on the queue" },
+            { name = "cache:clear", description = "Flush the application cache" },
+          }
+          commands = common_commands
+        end
+
+        pickers
+          .new({}, {
+            prompt_title = "Laravel Artisan Commands",
+            finder = finders.new_table({
+              results = commands,
+              entry_maker = function(entry)
+                return {
+                  value = entry,
+                  display = entry.name .. " - " .. entry.description,
+                  ordinal = entry.name .. " " .. entry.description,
+                }
+              end,
+            }),
+            sorter = conf.generic_sorter({}),
+            attach_mappings = function(prompt_bufnr, map)
+              actions.select_default:replace(function()
+                actions.close(prompt_bufnr)
+                local selection = action_state.get_selected_entry()
+                local cmd = "php artisan " .. selection.value.name
+                vim.ui.input({ prompt = "Artisan command: ", default = cmd }, function(input)
+                  if input then
+                    vim.cmd("!" .. input)
+                  end
+                end)
+              end)
+              return true
+            end,
+          })
+          :find()
       end,
-      expr = true,
-      noremap = true,
+      desc = "Artisan Commands (Fuzzy)",
     },
   },
-  event = { "VeryLazy" },
-  opts = {
-    lsp_server = "phpactor", -- "phpactor | intelephense"
-    features = {
-      pickers = {
-        provider = "snacks", -- "snacks | telescope | fzf-lua | ui-select"
+  config = function()
+    require("laravel").setup({
+      notifications = true,
+      debug = false,
+      keymaps = true, -- Enable default keymaps
+      sail = {
+        enabled = true,
+        auto_detect = true,
       },
-    },
-  },
+    })
+  end,
 }
